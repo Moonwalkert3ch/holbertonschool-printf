c0370ceeb99df82c9669b35d500b32234b2b6b51
Moonwalkert3ch <6620@holbertonstudents.com>
2023-07-10 16:32:16 -0700

    printf_reverse.c

---
+++ printf_reverse.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * printf_reverse - prints a binary number
+ * @args: number of arguments
+ * @printed_chars: the printed characters
+ * Return: printed characters
+*/
+
+int printf_reverse(va_list args, int printed_chars)
+{
+	char *str = va_arg(args, char *);
+	int len = 0, i;
+
+	while (str[len])
+	{
+		len++;
+	}
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(str[i]);
+		printed_chars++;
+	}
+
+	return (printed_chars);
+}
