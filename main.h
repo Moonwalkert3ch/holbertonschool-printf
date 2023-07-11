6bd85dcb723459c728fac80fdc315732795b3f60
Moonwalkert3ch <6620@holbertonstudents.com>
2023-07-11 09:12:57 -0700

    _printf.c

--- main.h
+++ main.h
@@ -7,16 +7,6 @@
 #include <limits.h>
 
 int _putchar(char c);
-int printf_char(va_list args, int printed_chars);
-int printf_string(va_list args, int printed_chars);
-int printf_numbers(va_list args, int printed_nums);
-int selector(const char *format, va_list args, int printed_chars);
-int printf_bin(unsigned int count, int printed_chars);
 int _printf(const char *format, ...);
-int printf_octa(unsigned int count, int printed_chars);
-int printf_unsigned(unsigned int count, int printed_chars);
-int printf_reverse(va_list args, int printed_chars);
-int printf_pointer(va_list args, int printed_chars);
-int printf_hexa(unsigned int count, int printed_chars, int upper_chars);
 
 #endif
