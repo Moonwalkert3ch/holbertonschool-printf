49f7f2c602b71701f9a8acd64b4fc9c6715077d7
Moonwalkert3ch <6620@holbertonstudents.com>
2023-07-11 09:29:08 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -16,6 +16,8 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	va_start(args, format);
+	if (format == NULL ||(format[0] == '%'))
+		return(-1);
 	while (*format != '\0')
 	{
 	if (*format == '%')
